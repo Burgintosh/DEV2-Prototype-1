6954e93dad6ddf9e0bbf7b639a54d32ad296a246
Burgess Leon Mackintosh Doan III <bdoaniii@gmail.com>
2026-04-15 08:06:18 -0400

    forgot to save before last commit

--- Dev2-Prototype1/Assets/Scripts/Weapon.cs
+++ Dev2-Prototype1/Assets/Scripts/Weapon.cs
@@ -59,7 +59,7 @@ public class Weapon : MonoBehaviour
         yield return new WaitForSeconds(.5f);
         if (bulletsLeft < magazineSize)
             bulletsLeft = magazineSize;
-        else if (bulletsLeft == magazineSize)
+        else if (bulletsLeft == magazineSize) 
             bulletsLeft = magazineSize + 1; // Changing this to bulletsLeft++ increases it by 1 for each frame it's reloading which is really funny.
         OnAmmoChange?.Invoke(bulletsLeft);
         isReloading = false;
