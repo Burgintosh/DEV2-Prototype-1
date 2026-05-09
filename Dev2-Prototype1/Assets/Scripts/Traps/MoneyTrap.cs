05b1ee3c6c4e54d9f9fae7a890986869ffc9e56f
stubcub <braunschweigerjacob6@gmail.com>
2026-05-09 02:50:13 -0400

    testable

--- Dev2-Prototype1/Assets/Scripts/Traps/MoneyTrap.cs
+++ Dev2-Prototype1/Assets/Scripts/Traps/MoneyTrap.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class MoneyTrap : MonoBehaviour
@@ -14,6 +15,107 @@ public class MoneyTrap : MonoBehaviour
     [Header("----- Refs -----")]
     [SerializeField] CurrencyManager currencyManager;
 
+    Coroutine payCoroutine;
+    bool isPayingOut;
 
+    private void Awake()
+    {
+        GetCurrenyManager();
+    }
+
+    private void OnEnable()
+    {
+        StartPayingPlayer();
+    }
+
+    private void OnDisable()
+    {
+        StopPayingPlayer();
+    }
+
+    void GetCurrenyManager()
+    {
+        if(currencyManager != null)
+        {
+            return;
+        }
+
+        if(gamemanager.instance != null)
+        {
+            currencyManager = gamemanager.instance.currencyManager;
+        }
+
+    }
+
+    void StartPayingPlayer()
+    {
+        isPayingOut = true;
+
+        if(payCoroutine != null)
+        {
+            StopCoroutine(payCoroutine);
+        }
+
+        payCoroutine = StartCoroutine(PayPlayerLoop());
+    }
+
+    void StopPayingPlayer()
+    {
+        isPayingOut = false;
+
+        if(payCoroutine == null)
+        {
+            return;
+        }
+
+        StopCoroutine(payCoroutine);
+        payCoroutine = null;
+    }
+
+    IEnumerator PayPlayerLoop()
+    {
+        while (isPayingOut)
+        {
+            yield return new WaitForSeconds(GetCurrentPayInterval());
+
+            if (isPayingOut)
+            {
+                PayPlayer();
+            }
+        }
+    }
+
+    void PayPlayer()
+    {
+        if(currencyManager == null)
+        {
+            Debug.LogWarning("[Money Trap] Currency manager is null", this);
+            return;
+        }
+
+        currencyManager.AddCurrency(GetCurrentAmountPerPay());
+    }
+
+    public int GetCurrentAmountPerPay()
+    {
+        return Mathf.RoundToInt(amountPerPayout * amountPerPayoutMult);
+    }
+
+    public float GetCurrentPayInterval()
+    {
+        float currentPayInterval = payoutInterval * payoutIntervalMult;
+
+        return Mathf.Max(minSecondsBetweenPayout, currentPayInterval);
+    }
+
+    public void SetAmountPerPayMult(float _NewAmountPerPayMult)
+    {
+        amountPerPayoutMult = _NewAmountPerPayMult;
+    }
+
+    public void SetPayoutIntervalMult(float _NewPayIntervalMult)
+    {
+        payoutIntervalMult = Mathf.Max(0.1f, _NewPayIntervalMult);
+    }
 
 }
