5bacf18e31a621118123244dfab89b719e9b593b
Burgess Leon Mackintosh Doan III <bdoaniii@gmail.com>
2026-05-12 16:11:51 -0400

    Added Credits Scene. Buttons now change color on hover, but still trying to change the color programmatically.

--- Dev2-Prototype1/Assets/Scripts/Menus/MainMenu.cs
+++ Dev2-Prototype1/Assets/Scripts/Menus/MainMenu.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
@@ -8,47 +9,84 @@ public class MainMenu : MonoBehaviour
     public GameObject EndlessMenu;
     public GameObject OptionsMenu;
     public GameObject ScoresMenu;
+    public GameObject CreditsMenu;
 
+    [SerializeField] GameObject menuParent;
+    [SerializeField] string hoverColorHex = "#8C8C8C";
+
+    private void Start()
+    {
+        Button[] buttons = GetComponentsInChildren<Button>(true);
+
+        foreach (Button btn in buttons)
+        {
+            ColorBlock colors = btn.colors;
+            Color hoverColor;
+            if (ColorUtility.TryParseHtmlString(hoverColorHex, out hoverColor))
+            {
+                colors.highlightedColor = hoverColor;
+            }
+            //colors.highlightedColor = 
+        }
+    }
+
+    // Open Menus
     public void OpenLevelSelectMenu()
     {
         mainMenu.SetActive(false);
         LevelSelectMenu.SetActive(true);
-    }
-    public void CloseLevelSelectMenu()
-    {
-        LevelSelectMenu.SetActive(false);
-        mainMenu.SetActive(true);
     }
     public void OpenEndlessMenu()
     {
         mainMenu.SetActive(false);
         EndlessMenu.SetActive(true);
     }
-    public void CloseEndlessMenu()
-    {
-        EndlessMenu.SetActive(false);
-        mainMenu.SetActive(true);
-    }
     public void OpenOptionsMenu()
     {
         mainMenu.SetActive(false);
         OptionsMenu.SetActive(true);
-    }
-    public void CloseOptionsMenu()
-    {
-        OptionsMenu.SetActive(false);
-        mainMenu.SetActive(true);
     }
     public void OpenScoresMenu()
     {
         mainMenu.SetActive(false);
         ScoresMenu.SetActive(true);
     }
+    public void OpenCreditsMenu()
+    {
+        mainMenu.SetActive(false);
+        CreditsMenu.SetActive(true);
+    }
+
+    // Close Menus
+    public void CloseLevelSelectMenu()
+    {
+        LevelSelectMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+    public void CloseEndlessMenu()
+    {
+        EndlessMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+    public void CloseOptionsMenu()
+    {
+        OptionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
     public void CloseScoresMenu()
     {
         ScoresMenu.SetActive(false);
         mainMenu.SetActive(true);
     }
+    public void CloseCreditsMenu()
+    {
+        CreditsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+
+
+    // Levels
     public void LoadLevelThree()
     {
         SceneManager.LoadScene("DiegoTestScene");
