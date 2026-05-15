1d30faf7f56d36ad08dd5e94960fd63e8b123e7c
Burgess Leon Mackintosh Doan III <bdoaniii@gmail.com>
2026-05-15 10:11:39 -0400

    Levels in Level Select and Endless Mode are locked until the previous main level is cleared.

--- Dev2-Prototype1/Assets/Scripts/Menus/LevelSelectionManager.cs
+++ Dev2-Prototype1/Assets/Scripts/Menus/LevelSelectionManager.cs
@@ -3,8 +3,7 @@ using UnityEngine.UI;
 
 public class LevelSelectionManager : MonoBehaviour
 {
-    [Header("Level Buttons")]
-    [SerializeField] private Button[] levelSelectButtonsButtons;
+    [SerializeField] private Button[] levelSelectButtons;
     [SerializeField] private Button[] endlessModeButtons;
     private const string UNLOCKED_LEVELS_KEY = "UnlockedLevels";
 
@@ -19,25 +18,24 @@ public class LevelSelectionManager : MonoBehaviour
     // checks it's position in the button array vs the number of unlocked levels.
     private void UpdateLockedButtons()
     {
-        if (levelSelectButtonsButtons.Length != endlessModeButtons.Length)
+        if (levelSelectButtons.Length != endlessModeButtons.Length)
         {
             Debug.LogWarning("The number of buttons in the Level Select Array does not match the number of buttons in the" +
                 " Endless Mode Array. Go to LevelSelectionManager.cs script on the MainMenuFunctions GameObject to resolve.");
         }
 
-        int unlockedLevels = PlayerPrefs.GetInt(UNLOCKED_LEVELS_KEY);
-        for (int i = 0; i < levelSelectButtonsButtons.Length; i++)
+        int unlockedLevels = PlayerPrefs.GetInt(UNLOCKED_LEVELS_KEY, 1);
+        for (int i = 0; i < levelSelectButtons.Length; i++)
         {
             if (i < unlockedLevels)
-            {
-                levelSelectButtonsButtons[i].interactable = true;
-                endlessModeButtons[i].interactable = true;
-            }
+                levelSelectButtons[i].interactable = true;
             else
-            {
-                levelSelectButtonsButtons[i].interactable = false;
+                levelSelectButtons[i].interactable = false;
+            
+            if (i + 1 < unlockedLevels) // Must clear the level first to unlock endless mode
+                endlessModeButtons[i].interactable = true;
+            else
                 endlessModeButtons[i].interactable = false;
-            }
         }
 
         //for (int i = 0; i < endlessModeButtons.Length; i++)
@@ -66,7 +64,8 @@ public class LevelSelectionManager : MonoBehaviour
 
     public void IncrementProgress()
     {
-        PlayerPrefs.SetInt(UNLOCKED_LEVELS_KEY + 1, 1);
+        int unlockedLevels = PlayerPrefs.GetInt(UNLOCKED_LEVELS_KEY);
+        PlayerPrefs.SetInt(UNLOCKED_LEVELS_KEY, unlockedLevels + 1);
         PlayerPrefs.Save();
         UpdateLockedButtons();
     }
--- Dev2-Prototype1/Assets/Scripts/buttonFunctions.cs
+++ Dev2-Prototype1/Assets/Scripts/buttonFunctions.cs
@@ -35,8 +35,6 @@ public class ButtonFunctions : MonoBehaviour
 
         if (sfxSlider != null)
             sfxSlider.value = SFXVol;
-
-
     }
 
 
